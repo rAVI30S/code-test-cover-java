35e24a6f204201bc5b9279a52707bd4d2b342332
Ravi <118107562+rAVI30S@users.noreply.github.com>
2024-07-10 10:19:36 +0530

    iteration 2

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,18 +1,60 @@
 package CodeTestCoverJava;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SoundexTest {
 
     @Test
     public void testEmptyString() {
-       //  assertTrue(Soundex.generateSoundex("")=="");
+        assertEquals("", Soundex.generateSoundex(""));
+    }
+
+    @Test
+    public void testNullString() {
+        assertEquals("", Soundex.generateSoundex(null));
     }
 
     @Test
     public void testSingleCharacter() {
-         //assertTrue(Soundex.generateSoundex("A")=="A000");
+        assertEquals("A000", Soundex.generateSoundex("A"));
+        assertEquals("Z000", Soundex.generateSoundex("Z"));
+    }
+
+    @Test
+    public void testStandardNames() {
+        assertEquals("R163", Soundex.generateSoundex("Robert"));
+        assertEquals("R163", Soundex.generateSoundex("Rupert"));
+        assertEquals("R150", Soundex.generateSoundex("Rubin"));
+        assertEquals("A261", Soundex.generateSoundex("Ashcraft"));
+        assertEquals("A261", Soundex.generateSoundex("Ashcroft"));
+        assertEquals("T522", Soundex.generateSoundex("Tymczak"));
+        assertEquals("P236", Soundex.generateSoundex("Pfister"));
+    }
+
+    @Test
+    public void testDifferentCasing() {
+        assertEquals("R163", Soundex.generateSoundex("robert"));
+        assertEquals("R163", Soundex.generateSoundex("RUPERT"));
+        assertEquals("A261", Soundex.generateSoundex("AshCrAfT"));
+    }
+
+    @Test
+    public void testNonAlphabeticCharacters() {
+        assertEquals("A000", Soundex.generateSoundex("A123"));
+        assertEquals("R163", Soundex.generateSoundex("R*obert"));
+        assertEquals("T522", Soundex.generateSoundex("Tymc!zak"));
+    }
+
+    @Test
+    public void testRepeatedLetters() {
+        assertEquals("W252", Soundex.generateSoundex("Woolworth"));
+        assertEquals("S560", Soundex.generateSoundex("Ssooonn"));
+    }
+
+    @Test
+    public void testVowelsBetweenConsonants() {
+        assertEquals("P234", Soundex.generateSoundex("Pencil"));
+        assertEquals("L530", Soundex.generateSoundex("Lunch"));
     }
 }
